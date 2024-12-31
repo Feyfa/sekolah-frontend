601874af1865d00d6c0bcea0595813c94b534a92
Feyfa <fisikamodern00@gmail.com>
2025-01-01 02:57:53 +0700

    remove env

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api',
-  baseURL: `${import.meta.env.VITE_APP_BACKEND_BASE_URL}/api`,
+  baseURL: `http://sekolah.backend.com/api`,
+  // baseURL: `${import.meta.env.VITE_APP_BACKEND_BASE_URL}/api`,
   timeout: 30000
 });
 
